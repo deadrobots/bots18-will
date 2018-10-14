58b51d0a749d43fbeb81d062b22e20419d9c7fdb
Will Bogusz <wbogusz24@gmail.com>
2018-10-14 16:09:57 -0400

    Started linefollow function

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ rMotor = 0
 armServo = 0
 clawServo = 1
 clawOpen = 1850
-clawClose = 1200
+clawClose = 1300
 armDown = 1300
 armUp = 0
+topHat = 1
 
 def freezeBoth():
     freeze(lMotor)
@@ -31,28 +32,45 @@ def wait_for_button():
     while right_button() == 1:
         print "Press Success"
 
-def main():
-    print "Let's Get Going"
-    enable_servos()
-    drive(95, 100, 1000)
-    drive(-95, -100, 1000)
-    for x in range(0, 4):
-        print(x)
-        drive(95, 100, 2000)
-        drive(100, 0, 1175)
-    freezeBoth()
-    msleep(300)
-    wait_for_button()
+def canGrab():
     set_servo_position(armServo, armDown)
     msleep(50)
     set_servo_position(clawServo, clawOpen)
     msleep(50)
-    drive(95, 100, 3000)
+    drive(90, 100, 3000)
     freezeBoth()
     set_servo_position(clawServo, clawClose)
     msleep(100)
     set_servo_position(armServo, armUp)
     msleep(1500)
+
+def main():
+    print "Let's Get Going"
+    enable_servos()
+
+    drive(95, 100, 1000)
+    drive(-95, -100, 1000)
+    for x in range(0, 4):
+        print(x)
+        drive(90, 100, 2000)
+        drive(100, 0, 1175)
+    freezeBoth()
+    msleep(300)
+
+    wait_for_button()
+
+    canGrab()
+
+    wait_for_button()
+
+    startTime = seconds()
+
+    while (seconds() - startTime) < 5:
+        if analog(topHat) > 2000:
+            drive(100, 20, 10)
+        else:
+            drive(20, 100, 10)
+    freezeBoth()
 
 if __name__ == "__main__":
     sys.stdout = os.fdopen(sys.stdout.fileno(), "w", 0)
